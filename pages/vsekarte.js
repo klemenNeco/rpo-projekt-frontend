cd3b23e2c2dcaeb594faecd2cffabb21d9c5fa30
dejant0 <tomincdejan@gmail.com>
2022-12-08 23:55:52 +0100

    fontawosome dodan

--- pages/vsekarte.js
+++ pages/vsekarte.js
@@ -1,11 +1,37 @@
 import Link from "next/link";
 import Layout from "../components/layout";
 import Navbar from "../components/navbar";
+import styles from "./vsekarte.module.css";
 
 const vsekarte = (props) => {
   return (
     <Layout>
       <Navbar />
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Štuk</a>
+          </li>
+          <li>
+            <a href="#">Trust</a>
+          </li>
+          <li>
+            <a href="#">po datomu</a>
+          </li>
+        </ul>
+      </nav>
+      <form>
+        <div className="search_btn">
+          <input
+            type="search"
+            placeholder="Ime zabave"
+            className="inputText"
+          ></input>
+          <button>
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+      </form>
     </Layout>
   );
 };
