2793ea28f413aea05ccbf185506f68c81bcfffa7
GabesSmallBrain <gabi.zadravec@gmail.com>
2022-12-14 19:35:49 +0100

    Fixed merge problems

--- components/navbar.js
+++ components/navbar.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import Layout from './layout';
-import styles from './navbar.module.css';
-
-export default function Navbar({ children }) {
-    return ( 
-        <Layout>
-            <div className={styles.navbar}>
-                <a href='/'><div className="logo">Ticket</div></a>
-                <nav className={styles.item}>
-                <ul className={styles.ul}>
-                    <li className={`${styles.left} ${styles.li}`}>
-                    <a href="/pages/vsekarte">Vse karte</a>
-                    </li>
-                    <li className={`${styles.prijava} ${styles.li}`}>
-                    <a href="/login">Prijava</a>
-                    </li>
-                    <li className={`${styles.registracija} ${styles.li}`}>
-                    <a href="/registration">Registracija</a>
-                    </li>
-                </ul>
-                </nav>
-            </div>
-        </Layout>
-    );
-}
-
-=======
 import Layout from "./layout";
 import styles from "./navbar.module.css";
 
@@ -60,4 +32,3 @@ export default function Navbar({ children }) {
     </Layout>
   );
 }
->>>>>>> main
