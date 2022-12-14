274c9d953ef17df255806861d845a6eb0641186f
GabesSmallBrain <gabi.zadravec@gmail.com>
2022-12-14 19:39:37 +0100

    More merge problems

--- pages/login.js
+++ pages/login.js
@@ -1,42 +1,5 @@
 import Link from "next/link";
 import Layout from "../components/layout";
-<<<<<<< HEAD
-import styles from "../styles/login.module.css"
-
-export default function Login() {
-    return (
-        <Layout>
-            <div>
-                <section id={styles.loginForm}>
-                    <h1>Prijava</h1>
-                    <form action="submit">
-                        <div>
-                            <label htmlFor="username">uporabniško ime</label>
-                            <input type="text" name="username" id="username" />
-                        </div>
-                        <div>
-                            <label htmlFor="password">geslo</label>
-                            <input type="password" name="password" id="password" />
-                        </div>
-                        <input type="submit" value="Prijava" id="submit" />
-                    </form>
-                </section>
-                <section id={styles.registration}>
-                    <div>Še nimaš profila? Registriraj se!</div>
-                    <hr />
-                    <div>
-                        ali pa se prijavi s pomočjo naslednjih metod
-                        <div>Google</div>
-                        <div>Facebook</div>
-                        <div>Apple</div>
-                    </div>
-                </section>
-                <h1><Link href="/">Nazaj domov</Link></h1>
-            </div>
-        </Layout> 
-    )
-}
-=======
 import styles from "../styles/login.module.css";
 import Navbar from "../components/navbar";
 
@@ -76,4 +39,3 @@ export default function Login() {
     </Layout>
   );
 }
->>>>>>> main
