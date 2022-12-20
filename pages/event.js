03a1082606dc191e4cad445ac01d635a9a48f835
GabesSmallBrain <gabi.zadravec@gmail.com>
2022-12-20 16:25:20 +0100

    Added carousels and some other stuff

--- pages/event.js
+++ pages/event.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { React, useState } from 'react'
 import Navbar from '../components/navbar';
 import Layout from '../components/layout';
+import Carousel from 'nuka-carousel'
    
 export default function Event() {
     const [counter, setCounter] = useState(0);
@@ -26,20 +27,43 @@ return (
     <div className={styles.top}>
         <div className={styles.toptext}><h1>Ime Dogodka</h1></div>
     </div>
-    <div className={styles.buycard}>
-        <div className={styles.right}>
-            <h2>Ime dogodka</h2>
-            <p>Lokacija cas datum</p>
-            Posebnosti karte (odrasli, otroci, ...)
-        </div>
-        <div className={styles.right}>
-            <h2>5€</h2>
-            <div className={styles.counter}>
-                Količina:
-                <button onClick={decrease}>-</button> 
-                <button className="counter__output">{counter}</button>
-                <button onClick={increase}>+</button>
+    <div className={styles.buycardparent}>
+        <div className={styles.buycard}>
+            <div className={styles.buycardtext}>
+                <div className={styles.right}>
+                    <h2>Ime dogodka</h2>
+                    <p>Lokacija cas datum</p>
+                    Posebnosti karte (odrasli, otroci, ...)
+                </div>
+                <div className={styles.right}>
+                    <h2>5€</h2>
+                    
+                    Količina:
+                    <div className={styles.counter}>
+                        <button onClick={decrease}>-</button> 
+                        <button className="counter__output">{counter}</button>
+                        <button onClick={increase}>+</button>
+                        <button className={styles.kupi}>Kupi Zdaj</button>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
+    <div className={styles.opisbody}>
+        <div className={styles.opis}>
+            <h2>Kratek opis dogodka</h2>
+            <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare elementum risus nec sagittis. In hac habitasse platea dictumst. Suspendisse vehicula libero eu magna luctus, vel interdum nunc commodo. 
+            </div>
+        </div>
+        <div className={styles.carousel}>
+            <Carousel defaultControlsConfig={{nextButtonText: '->', prevButtonText: '<-', pagingDotsStyle: {fill: 'none'}}} wrapAround={true}>
+                <img src="https://cdn.britannica.com/85/139485-050-BCF84C18/FIFA-World-Cup-trophy.jpg" height={200}/>
+                <img src="https://culturedvultures.com/wp-content/uploads/2022/08/Black-Clover.jpg" height={200} />
+                <img src="/backgroundImg.png" />
+                <img src="slike/backgroundImg.png" />
+                <img src="/slike/backgroundImg.png" />
+            </Carousel>
         </div>
     </div>
     </section>
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,12 @@ import Link from 'next/link';
 import Navbar from '../components/navbar';
 import CarouselCard from "../components/carouselcard";
 import Layout from '../components/layout';
+import Carousel from 'nuka-carousel'
+import { Cabin } from '@next/font/google'
+
+const cabin = Cabin({
+  subsets: ['latin'],
+})
 
 export default function Home() {
   return (
@@ -14,7 +20,7 @@ export default function Home() {
         <div className={styles.toptext}><h1>Vstopnice za vse prireditve. <br /> Na enem mestu</h1></div>
       </div>
       <h1 className={styles.carouselname}>Štuk</h1>
-      <div className={styles.carousel}>
+      <Carousel defaultControlsConfig={{nextButtonText: '->', prevButtonText: '<-', pagingDotsStyle: {fill: 'none'}}} nextButtonClassName="next" className={styles.carousel} slidesToShow={4}>
         <a href="/event"><CarouselCard /></a>
         <a href="/event"><CarouselCard /></a>
         <a href="/event"><CarouselCard /></a>
@@ -22,7 +28,34 @@ export default function Home() {
         <a href="/event"><CarouselCard /></a>
         <a href="/event"><CarouselCard /></a>
         <a href="/event"><CarouselCard /></a>
-        </div>
+      </Carousel>
+
+
+      <h1 className={styles.carouselname}>Trust</h1>
+      <Carousel defaultControlsConfig={{nextButtonText: '->', prevButtonText: '<-', pagingDotsStyle: {fill: 'none'}}} className={styles.carousel} slidesToShow={4}>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+      </Carousel>
+
+
+      <h1 className={styles.carouselname}>KMŠ</h1>
+      <Carousel defaultControlsConfig={{nextButtonText: '->', prevButtonText: '<-', pagingDotsStyle: {fill: 'none'}}} className={styles.carousel} slidesToShow={4}>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+        <a href="/event"><CarouselCard /></a>
+      </Carousel>
+      <div className={styles.buttonparent}>
+      <a href="/vsekarte" className={styles.button}>Vsi Dogodki</a>
+      </div>
       </section>
     </div>
     </Layout>
