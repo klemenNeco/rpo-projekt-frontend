9bc28d039a4a1c99e78a395b7ae93ae3ab7e8925
GabesSmallBrain <gabi.zadravec@gmail.com>
2023-01-18 20:29:10 +0100

    Changed navbar to look like intended

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,7 @@
 import Layout from "./layout";
 import styles from "./navbar.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faUser, faHouse} from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar({ children }) {
   return (
@@ -12,22 +14,37 @@ export default function Navbar({ children }) {
         }
       `}</style>
       <div className={styles.navbar}>
-        <a href="/" className="logo">
-          vstopnice.si
-        </a>
         <nav className={styles.item}>
-          <ul className={styles.ul}>
+          <div className={styles.left}>       
+            <a href="/" className={styles.logo}>
+              vstopnice.si
+            </a>
+            <ul className={styles.ul}>
             <li className={`${styles.left} ${styles.li}`}>
-              <a href="/vsekarte">Vse karte</a>
-            </li>
-            <li className={`${styles.prijava} ${styles.li}`}>
-              <a href="/login">Prijava</a>
-            </li>
-            <li className={`${styles.registracija} ${styles.li}`}>
-              <a href="/register">Registracija</a>
-            </li>
-          </ul>
+                <p className={styles.divide}>|</p>
+              </li>
+              <li className={`${styles.left} ${styles.li}`}>
+                <a href="/vsekarte">Vse karte</a>
+              </li>
+              <li className={`${styles.prijava} ${styles.li}`}>
+                <a href="/login">Štuk</a>
+              </li>
+              <li className={`${styles.registracija} ${styles.li}`}>
+                <a href="/register">Trust</a>
+              </li>
+            </ul>          
+          </div>
         </nav>
+        <div className={styles.right}>
+              <ul className={styles.ul}>
+                <li className={`${styles.right} ${styles.li}`}>
+                  <a href="/"><FontAwesomeIcon icon={faHouse} /></a>
+                </li>
+                <li className={`${styles.right} ${styles.li}`}>
+                  <a href="/register"><FontAwesomeIcon icon={faUser} /></a>
+                </li>
+              </ul>          
+            </div>
       </div>
     </Layout>
   );
